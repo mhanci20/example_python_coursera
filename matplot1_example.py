c22c49fbe5a39a7b727869ff32a5da0ce71f8673
My name <me@example.com>
2024-12-02 02:35:50 +0300

    matplot1_example.py eklendi

---
+++ matplot1_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+ypoints = np.array([2, 4, 1, 5])
+
+# Plotting without x-points
+plt.figure()
+
+plt.plot(ypoints)  # y axis
+
+plt.show()
