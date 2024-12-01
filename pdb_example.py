c7b04767bf99216b31f5c7aea5a30abcb3cf819c
My name <me@example.com>
2024-12-02 02:39:13 +0300

    pdb_example.py eklendi

---
+++ pdb_example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pdb
+
+def add_numbers(a, b):
+    pdb.set_trace() # This will set a breakpoint in the code
+    result = a + b
+    return result
+
+
+print(add_numbers(3, 4))
