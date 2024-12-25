22818fb032692fe359329e1e681552047121fe48
mehmet <mhanci20@gmail.com>
2024-12-25 12:45:49 +0300

    python_example_02.py added

---
+++ python_example_02.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# astr = 'Hello.py'
+# try:
+#    istr = int(astr)
+
+# except:
+#       istr = -1
+
+# print('First', istr)
+
+# astr = '123'
+# try:
+#    istr = int(astr)
+
+# except:
+#       istr = -1
+
+# print('Second', istr)
+
+astr = 'Bob'
+try:
+   print('Hello')
+   istr = int(astr)
+   print('There')
+except:
+      istr = -1
+
+print('Done', istr)
