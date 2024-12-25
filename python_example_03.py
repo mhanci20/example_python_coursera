a80b3feee27be8c422fdb197461e069cf76240db
mehmet <mhanci20@gmail.com>
2024-12-25 12:49:13 +0300

    python_example_03.py added

---
+++ python_example_03.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+rawstr = input('Enter a number:')
+try:
+   ival = int(rawstr)
+except:
+      ival = -1
+
+if ival > 0:
+    print('Nice work')
+else:
+    print('Not a number')
