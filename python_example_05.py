3d626e3bacb25d10d23831646fb9e60af2c645ac
mehmet <mhanci20@gmail.com>
2024-12-25 13:17:42 +0300

    python_example_05.py added

---
+++ python_example_05.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+#print(float(99) / 100) [Type Conversions Subjects]
+#i = 42
+#print(type(i))
+#f = float(i)
+#print(f)
+#print(type(f))
+
+#print(1 + 2 * float(3) / 4 - 5) [Type Conversion Subject]
+
+#sval = '123' [String Conversions]
+#print(type(sval))
+#print(sval + 1)
+#ival = int(sval)
+#print(type(ival))
+#print(ival + 1)
+#nsv = 'hello bob'
+#niv = int(nsv) [String Conversions]
+
+x = 5
+#print('Hello')
+
+def print_lyrics():
+    print("I'm a lumberjack and I'm okay.")
+    print('I sleep all night and I work all day.')
+
+print('Yo')
+print_lyrics()
+x = x + 2
+print(x)
+
