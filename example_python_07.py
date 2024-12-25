f95736ccd74c90e3a42f8b4d705ce27fc1349196
mehmet <mhanci20@gmail.com>
2024-12-25 13:38:36 +0300

    example_python_07.py added

---
+++ example_python_07.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+#big = max('Hello world')
+
+#print(big)
+#def max(inp):
+#    blah
+#    blah
+#    return 'm'
+
+#def addtwo(a, b):
+#    added = a + b
+#    return added
+
+#x = addtwo(3, 5)
+#print(x)
+
+#n = 5
+#while n > 5:
+#    print(n)
+#    n = n - 1
+#print('Blastoff!')
+#print(n)
+
+# n = 5
+#while n > 0:
+#    print('Lather')
+#    print('Rinse')
+#print('Dry off')
+
+#n = 5
+#while n > 0:
+#    print('lather')
+#    print('Rinse')
+#    break
+#print('Dry off')
+
+#n = 0
+#while n > 0:
+#    print('Lather')
+#    print('Rinse')
+#print('Dry off')
+
+#while True:
+#    line = input('> ')
+#    if line == 'done':
+#        break
+#    print(line)
+#print('Done!')
+
+#while True:
+#    line = input('> ')
+#    if line[0] == '#' :
+#        continue
+#    if line == 'done' :
+#        break
+#    print(line)
+#print('Done!')
+
+#for i in [5, 4, 3, 2, 1] :
+#    print(i)
+#print('Blastoff!')
+
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends:
+    print('Happy New Year', friend)
+print('Done!')
