95aadb8b8576bcb39a10acc09cff6bb93f78b099
mehmet <mhanci20@gmail.com>
2024-12-27 20:42:38 +0300

    example_python_11.py added

---
+++ example_python_11.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+largest = None
+smallest = None
+
+while True:
+    num = input('Enter a number: ')
+    if num == 'done':
+        break
+    try:
+       n = int(num)
+       if largest is None or n > largest:
+           largest = n
+       if smallest is None or n < smallest:
+           smallest = n
+    except ValueError:
+        print("Invalid input")
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
