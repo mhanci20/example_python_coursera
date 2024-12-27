b0ffb0f0fd7eabae37bdfa8710fe8fd36fe236a8
mehmet <mhanci20@gmail.com>
2024-12-27 20:36:51 +0300

    example_python_10.py added

---
+++ example_python_10.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+num = 0
+tot = 0.0
+while True:
+    sval = input('Enter a number: ')
+    if sval ==  'done':
+        break
+    try:
+       fval = float(sval)
+    except:
+        print('Invalid input')
+        continue
+    # print(fval)
+    num = num + 1
+    tot = tot + fval
+
+# print('ALL DONE')
+print(tot,num,tot/num)
